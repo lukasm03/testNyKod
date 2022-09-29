3e1bf1097f08271cfa3b237e63a992d6fe5165ea
Lukas Mörtman <lukas@mocko.se>
2022-09-29 18:15:11 +0200

    fixade env

--- utils/laddaUppBild.js
+++ utils/laddaUppBild.js
@@ -4,8 +4,8 @@ import { PutObjectCommand, S3Client  } from "@aws-sdk/client-s3";
 //initiera variabler och ladda in värden från bucket
 const namnpåBucket = process.env.BUCKET_NAME
 const region = process.env.BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID_LUKAS
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY_LUKAS
 
 export default async function laddaUppBild(fil) {
   //anslut till lagringen
